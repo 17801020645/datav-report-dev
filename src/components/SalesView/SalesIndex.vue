29cfc90af87b4f06c550ba4f9317187fbef84b64
yuhu <17801020645@163.com>
2025-09-09 16:32:06 +0800

    销售趋势图表和列表开发  done

--- src/components/SalesView/SalesIndex.vue
+++ src/components/SalesView/SalesIndex.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sales-view">
-    <el-card shadow="hover">
+    <el-card shadow="hover" :body-style="{ padding: '0 0 20px 0' }">
       <template v-slot:header>
         <div class="menu-wrapper">
           <el-menu
@@ -34,17 +34,32 @@
         </div>
       </template>
 
-      <div>222</div>
-      <div>222</div>
-      <div>444</div>
-
-      <div>333</div>
+      <div class="sales-view-chart-wrapper">
+        <v-chart class="chart" :option="option" autoresize />
+        <div class="sales-view-list">
+          <div class="sales-view-title">排行榜</div>
+          <div class="list-item-wrapper">
+            <div class="list-item" v-for="item in rankData" :key="item.no">
+              <!-- <div class="list-item-no" :class="+item.no <= 3 ? 'top-on' : ''">{{ item.no }}</div> -->
+              <div :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']">{{ item.no }}</div>
+              <div class="list-item-name">{{ item.name }}</div>
+              <div class="list-item-money">{{ item.money }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </el-card>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, watchEffect } from 'vue'
+import { use } from 'echarts/core'
+import { BarChart } from 'echarts/charts'
+import { GridComponent, DatasetComponent, TitleComponent } from 'echarts/components'
+import VChart from 'vue-echarts'
+
+use([BarChart, DatasetComponent, GridComponent, TitleComponent])
 
 const activeIndex = ref<string>('1')
 const radioSelect = ref<string>('今日')
@@ -76,6 +91,112 @@ const shortcuts = [
       start.setTime(start.getTime() - 3600 * 24 * 1000 * 90)
       return [start, end]
     },
+  },
+]
+
+const option = ref({
+  title: {
+    text: '年度销售额',
+    textStyle: {
+      fontSize: 12,
+      color: '#666',
+    },
+    left: 25,
+    top: 20,
+  },
+  xAxis: {
+    type: 'category',
+    data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    axisTick: {
+      alignWithLabel: true,
+      lineStyle: {
+        color: '#999',
+      },
+    },
+    axisLine: {
+      lineStyle: {
+        color: '#999',
+      },
+    },
+    axisLabel: {
+      color: '#333',
+    },
+  },
+  yAxis: {
+    axisLine: {
+      show: false,
+    },
+    axisTick: {
+      show: false,
+    },
+    splitLine: {
+      lineStyle: {
+        type: 'dotted',
+        color: '#eee',
+      },
+    },
+  },
+  series: [
+    {
+      type: 'bar',
+      barWidth: '35%',
+      data: [200, 250, 300, 350, 300, 250, 200, 250, 300, 350, 300, 250],
+    },
+  ],
+  color: ['#3398DB'],
+  grid: {
+    top: 70,
+    left: 60,
+    right: 60,
+    bottom: 50,
+  },
+})
+
+const rankData = [
+  {
+    no: 1,
+    name: '麦当劳',
+    money: '323,234',
+  },
+  {
+    no: 2,
+    name: '麦当劳',
+    money: '323,234',
+  },
+  {
+    no: 3,
+    name: '麦当劳',
+    money: '323,234',
+  },
+  {
+    no: 4,
+    name: '麦当劳',
+    money: '323,234',
+  },
+  {
+    no: 5,
+    name: '麦当劳',
+    money: '323,234',
+  },
+  {
+    no: 6,
+    name: '麦当劳',
+    money: '323,234',
+  },
+  {
+    no: 7,
+    name: '麦当劳',
+    money: '323,234',
+  },
+  {
+    no: 8,
+    name: '麦当劳',
+    money: '323,234',
+  },
+  {
+    no: 9,
+    name: '麦当劳',
+    money: '323,234',
   },
 ]
 
@@ -117,6 +238,61 @@ function onMenuSelect(key: string) {
       //   // width: 280px !important;
       //   margin-left: 20px;
       // }
+    }
+  }
+  .sales-view-chart-wrapper {
+    display: flex;
+    height: 270px;
+    .chart {
+      flex: 0 0 70%;
+      width: 70%;
+      height: 100%;
+    }
+    .sales-view-list {
+      flex: 1;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+
+      .sales-view-title {
+        margin-top: 20px;
+        font-size: 12px;
+        color: #666;
+        font-weight: 500;
+      }
+      .list-item-wrapper {
+        margin-top: 15px;
+        .list-item {
+          display: flex;
+          align-items: center;
+          font-size: 12px;
+          height: 20px;
+          padding: 6px 20px 6px 0;
+          .list-item-no {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 20px;
+            height: 20px;
+            color: #333;
+            &.top-no {
+              background: #000;
+              border-radius: 50%;
+              color: #fff;
+              font-weight: 500;
+            }
+          }
+          .list-item-name {
+            margin-left: 10px;
+            color: #333;
+          }
+
+          .list-item-money {
+            flex: 1;
+            text-align: right;
+          }
+        }
+      }
     }
   }
 }
