e9a0723c7fd6722f927e1f8547ad9e6d9f2d2eef
yuhu <17801020645@163.com>
2025-09-09 15:11:56 +0800

    销售趋势组件容器 done

--- src/components/SalesView/SalesIndex.vue
+++ src/components/SalesView/SalesIndex.vue
@@ -3,7 +3,12 @@
     <el-card shadow="hover">
       <template v-slot:header>
         <div class="menu-wrapper">
-          <el-menu mode="horizontal" :default-active="activeIndex" @select="onMenuSelect">
+          <el-menu
+            mode="horizontal"
+            :default-active="activeIndex"
+            @select="onMenuSelect"
+            class="sales-view-menu"
+          >
             <el-menu-item index="1">销售额</el-menu-item>
             <el-menu-item index="2">访问量</el-menu-item>
           </el-menu>
@@ -21,7 +26,9 @@
               start-placeholder="开始日期"
               end-placeholder="结束日期"
               size="small"
+              unlink-panels
               :shortcuts="shortcuts"
+              class="sales-view-date-picker"
             />
           </div>
         </div>
@@ -42,10 +49,6 @@ import { ref, watchEffect } from 'vue'
 const activeIndex = ref<string>('1')
 const radioSelect = ref<string>('今日')
 const date = ref<unknown>()
-// const pickerOptions = ref({
-
-// })
-
 const shortcuts = [
   {
     text: '最近一周',
@@ -76,29 +79,45 @@ const shortcuts = [
   },
 ]
 
+// TODO 用不到删除
 watchEffect(() => console.log(activeIndex.value))
 watchEffect(() => console.log(radioSelect.value))
 
 function onMenuSelect(key: string) {
   activeIndex.value = key
 }
-
-// const pickerOptions = computed(() => {
-//   return {
-//     shortcuts: [
-//       {
-//         text: '最近一周',
-//       },
-//     ],
-//   }
-// })
 </script>
 
 <style scoped lang="scss">
 .sales-view {
   margin-top: 20px;
   .menu-wrapper {
+    position: relative;
     display: flex;
+    .sales-view-menu {
+      width: 100%;
+      padding-left: 20px;
+      .el-menu-item {
+        height: 50px;
+        line-height: 50px;
+        margin: 0 20px;
+      }
+    }
+    .menu-right {
+      position: absolute;
+      top: 5px;
+      right: 20px;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      gap: 20px;
+      // flex-shrink: 0;
+      // justify-content: flex-end;
+      // .sales-view-date-picker {
+      //   // width: 280px !important;
+      //   margin-left: 20px;
+      // }
+    }
   }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import './style/index.css'
 
 const pinia = createPinia()
 const app = createApp(App)
